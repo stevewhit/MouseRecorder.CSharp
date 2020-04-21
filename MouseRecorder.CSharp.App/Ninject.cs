f2f72f9e280fd157ed83e9bdc706635f732b249f
Steve Whitmire <swhit114@gmail.com>
2020-04-20 20:30:44 -0400

    Initial project creation and setup of referenced projects;

---
+++ MouseRecorder.CSharp.App/Ninject.cs
@@ -0,0 +1,26 @@
+﻿using Framework.Generic.EntityFramework;
+using log4net;
+using Ninject;
+using Ninject.Modules;
+
+namespace MouseRecorder.CSharp.App
+{
+    public class NinjectBindings : NinjectModule
+    {
+        public override void Load()
+        {            
+            //var context = new EfContext(new SMAContext());
+
+            //Bind<IEfRepository<Company>>().ToMethod(_ => new EfRepository<Company>(context));
+            //Bind<IEfRepository<Quote>>().ToMethod(_ => new EfRepository<Quote>(context));
+
+            //Bind<ICompanyService<Company>>().ToConstructor(_ => new CompanyService<Company>(Kernel.Get<IEfRepository<Company>>())).InThreadScope();
+            //Bind<IQuoteService<Quote>>().ToConstructor(_ => new QuoteService<Quote>(Kernel.Get<IEfRepository<Quote>>())).InThreadScope();
+            //Bind<IMarketDownloader<Company, Quote>>().ToConstructor(_ => new EXCloudWrapper(token)).InThreadScope();
+
+            //Bind<IMarketService<Company, Quote>>().ToConstructor(_ => new MarketService<Company, Quote>(Kernel.Get<ICompanyService<Company>>(), Kernel.Get<IQuoteService<Quote>>(), Kernel.Get<IMarketDownloader<Company, Quote>>()));
+
+            //Bind<ILog>().ToMethod(_ => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType));
+        }
+    }
+}
