74c7d434c00871a7b7a9007d2288e8c3a655b485
Steve Whitmire <swhit114@gmail.com>
2020-04-20 22:05:40 -0400

    [QR-1] - Added database creation scripts; Added data model and data model test projects; Updated gitignore;

---
+++ MouseRecorder.CSharp.DataModel.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MouseRecorder.CSharp.DataModel.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MouseRecorder.CSharp.DataModel.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba59e71b-c152-4a5b-b235-8ea6fde06eac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
