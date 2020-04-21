99308ef24ae2bdb9e6455d0792193bb7378f8a34
Steve Whitmire <swhit114@gmail.com>
2020-04-20 22:06:30 -0400

    [QR-2] - Configured Log4Net for application logging;

--- MouseRecorder.CSharp.App/Ninject.cs
+++ MouseRecorder.CSharp.App/Ninject.cs
@@ -1,26 +1,17 @@
-﻿using Framework.Generic.EntityFramework;
-using log4net;
-using Ninject;
+﻿using log4net;
 using Ninject.Modules;
+using MouseRecorder.CSharp.DataModel;
+using Framework.Generic.EntityFramework;
 
 namespace MouseRecorder.CSharp.App
 {
     public class NinjectBindings : NinjectModule
     {
         public override void Load()
-        {            
-            //var context = new EfContext(new SMAContext());
+        {
+            var context = new EfContext(new MRContext());
 
-            //Bind<IEfRepository<Company>>().ToMethod(_ => new EfRepository<Company>(context));
-            //Bind<IEfRepository<Quote>>().ToMethod(_ => new EfRepository<Quote>(context));
-
-            //Bind<ICompanyService<Company>>().ToConstructor(_ => new CompanyService<Company>(Kernel.Get<IEfRepository<Company>>())).InThreadScope();
-            //Bind<IQuoteService<Quote>>().ToConstructor(_ => new QuoteService<Quote>(Kernel.Get<IEfRepository<Quote>>())).InThreadScope();
-            //Bind<IMarketDownloader<Company, Quote>>().ToConstructor(_ => new EXCloudWrapper(token)).InThreadScope();
-
-            //Bind<IMarketService<Company, Quote>>().ToConstructor(_ => new MarketService<Company, Quote>(Kernel.Get<ICompanyService<Company>>(), Kernel.Get<IQuoteService<Quote>>(), Kernel.Get<IMarketDownloader<Company, Quote>>()));
-
-            //Bind<ILog>().ToMethod(_ => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType));
+            Bind<ILog>().ToMethod(_ => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType));
         }
     }
 }
--- MouseRecorder.CSharp.App/Program.cs
+++ MouseRecorder.CSharp.App/Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using log4net;
+using Ninject;
+using System.Reflection;
+using System;
 
 namespace MouseRecorder.CSharp.App
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            var log = kernel.Get<ILog>();
+
+            try
+            {
+                log.Info("Starting..");
+            }
+            catch (AggregateException e)
+            {
+                LogRecursive(log, e, "Error occured.");
+            }
+
+            log.Info("Finished..");
+            kernel.Dispose();
+        }
+
+        /// <summary>
+        /// Logs the inner exceptions of an AggregateException separately.
+        /// </summary>
+        private static void LogRecursive(ILog log, AggregateException e, string message)
+        {
+            foreach (var innerException in e.InnerExceptions)
+            {
+                if (innerException is AggregateException)
+                    LogRecursive(log, innerException as AggregateException, message);
+                else
+                    log.Error($"{message}", innerException);
+            }
         }
     }
 }
